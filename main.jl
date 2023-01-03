2ee8810359c86d0fcf90045310d62d564e61def9
Héloïse Gachet <gachetheloise@gmail.com>
2023-01-03 20:34:58 +0100

    suppression de fichier effectuee, plans coupants sans bugs

--- main.jl
+++ main.jl
@@ -1,9 +1,11 @@
 include("dualisation.jl")
 include("PL_statique.jl")
+include("plan_coupants.jl")
 
 function main()
     #PL_statique("data/10_ulysses_3.tsp")
-    dualisation("data/10_ulysses_3.tsp")
+    #dualisation("data/10_ulysses_3.tsp")
+    plan_coupants("data/10_ulysses_3.tsp")
 end
 
 main()
--- plan_coupants.jl
+++ plan_coupants.jl
@@ -1,14 +1,25 @@
-function plan_coupants()
-    U1 = init_U1()
+function plan_coupants(filename)
+    include(filename)
+    l = zeros(Float64, n, n)
+    for i in 1:n
+        for j in 1:n
+            l[i,j] = sqrt((coordinates[i,1] - coordinates[j,1])^2
+                            +(coordinates[i,2] - coordinates[j,2])^2)
+    
+        end
+    end
+    epsilon = 10^-6
+
+    U1 = init_U1(l)
     U2 = init_U2()
     violation = true
-    while(violiation)
+    while(violation)
         violation = false
-        x_star, y_star, z_star = PM(U1, U2) 
-        delta1_star = SP1(x_star)
-        delta2_star = Vector{Float64}(0,3)
-        delta2_star[k] = SP2k(k,y_star)
-        if(sum(x_star[i,j]*(l[i,j]+delta1[i,j]*(lh[i]+lh[j]))) - z_star > epsilon)
+        x_star, y_star, z_star = PM(U1, U2,l) 
+        delta1_star = SP1(x_star, l)
+        
+        if(sum(x_star[i,j]*(l[i,j]+delta1_star[i,j]*(lh[i]+lh[j])) for i in 1:n for j in 1:n) - z_star > epsilon)
+            println("violation de contrainte sur les distances")
             l1 = zeros(Float64, n, n)
             for i in 1:n
                 for j in 1:n
@@ -19,32 +30,36 @@ function plan_coupants()
             violation = true
         end
         for k in 1:K
-            if(sum(y_star[i,k]*w_v[i]*(1+delta2_star[k][i]) - B > epsilon))
-                w2 = Vector{Float64}(0, n)
+            delta2_star = SP2k(k, y_star)
+            if(sum(y_star[i,k]*w_v[i]*(1+delta2_star[i]) for i in 1:n) - B > epsilon)
+                println("violation de contrainte sur les poids")
+                w2 = Vector{Float64}(undef, n)
                 for i in 1:n
-                    w2[i] = w_v[i]*(1+delta2_star[k][i])
+                    w2[i] = w_v[i]*(1+delta2_star[i])
                 end
                 push!(U2,w2)
                 violation = true
             end
         end
+    end
 end
 
-function init_U1()
-    U1 = Vector{Float64}()
+function init_U1(l)
+    U1 = Vector{Matrix{Float64}}()
     push!(U1, l)
     return U1
 end
 
-function init_U1()
-    U2 = Vector{Float64}()
+function init_U2()
+    U2 = Vector{Vector{Float64}}()
     push!(U2, w_v)
     return U2
 end
 
-function PM(U1, U2)
+function PM(U1, U2, l)
     # Create the model
     m = Model(CPLEX.Optimizer)
+    set_optimizer_attribute(m, "CPX_PARAM_SCRIND", 0)
 
     ### Variables
     @variable(m, x[i in 1:n, j in 1:n], Bin)
@@ -60,15 +75,26 @@ function PM(U1, U2)
     
     ### Objective
     @objective(m, Min, sum(l[i,j]*x[i,j] for i in 1:n, j in 1:n if i<j))
-
-
     ### Solve the problem
-    start = time()
     optimize!(m)
+
+    x_val = zeros(Int64, n, n)
+    y_val = zeros(Int64, n, K)
+    for i in 1:n
+        for j in 1:n
+            x_val[i,j] = JuMP.value(x[i,j])
+        end
+        for k in 1:K
+            y_val[i,k] = JuMP.value(y[i,k])
+        end
+    end
+
+    return x_val, y_val, JuMP.value(z)
 end
 
-function SP1(x_star)
+function SP1(x_star, l)
     m1 = Model(CPLEX.Optimizer)
+    set_optimizer_attribute(m1, "CPX_PARAM_SCRIND", 0)
 
     ### Variables
     @variable(m1, 3>=delta1[i in 1:n, j in 1:n]>=0)
@@ -81,11 +107,18 @@ function SP1(x_star)
     
     ### Solve the problem
     optimize!(m1)
-    return JuMP.value(delta1)
+    delta_val = zeros(Float64, n, n)
+    for i in 1:n
+        for j in 1:n
+            delta_val[i,j] = JuMP.value(delta1[i,j])
+        end
+    end
+    return delta_val
 end
 
 function SP2k(k, y_star)
     m2 = Model(CPLEX.Optimizer)
+    set_optimizer_attribute(m2, "CPX_PARAM_SCRIND", 0)
 
     ### Variables
     @variable(m2, W_v[i]>=delta2[i in 1:n]>=0)
@@ -97,6 +130,6 @@ function SP2k(k, y_star)
     @objective(m2, Max, sum(w_v[i]*y_star[i,k]+w_v[i]*delta2[i]*y_star[i,k] for i in 1:n))
     
     ### Solve the problem
-    optimize!(m2)
-    return JuMP.value(delta2)
+    optimize!(m2)    
+    return Vector{Float64}([JuMP.value(delta2[i]) for i in 1:n])
 end
