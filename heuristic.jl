ce2feaf49969a2688db528cc5a7e641a090b2bc6
Héloïse Gachet <gachetheloise@gmail.com>
2023-01-29 13:44:41 +0100

    merge

--- heuristic.jl
+++ heuristic.jl
@@ -1,3 +1,7 @@
+using JuMP
+using CPLEX
+
+
 function added_value(i, partie, l)
 	# augmentation du coût de la solution en ajoutant i à la partie
 	v = 0
@@ -5,6 +9,37 @@ function added_value(i, partie, l)
 		v = v + l[i,j]
 	end
 	return v
+end
+
+function value_sol(partition, l)
+	
+	# Create the model
+	m = Model(CPLEX.Optimizer)
+	### Variables
+	@variable(m, delta[i in 1:n, j in 1:n]>=0)
+	### Constraints
+	@constraint(m, [i in 1:n, j in 1:n;i!=j], delta[i,j]<=3)
+	@constraint(m, sum(delta[i,j] for i in 1:n, j in 1:n if i!=j) <=L)
+	### Objective
+	obj = 0
+	for index_k in 1:length(partition)
+		if (length(partition[index_k]) >= 2)
+			obj += sum(l[i,j] - 3*(lh[i]+lh[j]) + delta[i,j]*(lh[i]+lh[j]) for i in partition[index_k], j in partition[index_k] if i!=j) 
+		end
+	end
+	
+	@objective(m, Max, obj)
+	### Solve the problem
+	start = time()
+	optimize!(m)
+	stop = time()
+	println(" solution of obj value ", JuMP.objective_value(m),"solving time = ",stop - start, "s")
+	#for i in 1:n
+	#	for j in 1:n
+	#		println(i," ",j," l ", l[i,j]-3*(lh[i]+lh[j]), " delta ", value(delta[i,j]))
+	#	end
+	#end
+	return JuMP.objective_value(m)
 end
 
 
@@ -59,6 +94,7 @@ function heuristic(filepath)
 			if w_group[k] + w_nodes[i] > B
 				continue
 			else
+				#println(partition[k],l)
 				v = added_value(i, partition[k], l)
 				if v < min_added_value
 					min_added_value = v
@@ -76,28 +112,38 @@ function heuristic(filepath)
 			push!(partition[best_k], i)
 			assignment[i] = best_k
 			w_group[best_k] = w_group[best_k] + w_nodes[i]
-			v_group[best_k] = v_group[best_k] + min_added_value
-			v_sol = v_sol + min_added_value
+			v_group[best_k] += min_added_value
+			v_sol += min_added_value
 		end
 	end
-
+	v_sol = value_sol(partition, l)
 	return partition, v_sol
+end
+
+filename = "data_small/10_ulysses_3.tsp"
+P, v = heuristic(filename)
+if v == 0
+	println("erreur")
+else
+	println("partition = ", P)
+	println("valeur = ", v)
 end
 
 # application à toutes les instances
 
-fout = open("output_heuristic.txt", "w")
-
-for file in readdir("./data")
-	println(fout, "instance " * file)
-	P, v = heuristic("./data/" * file)
-	if v == 0
-		println(fout, "erreur")
-	else
-		println(fout, "partition = ", P)
-		println(fout, "valeur = ", v)
-	end
-	println(fout, "")
-end
-
-close(fout)
+#fout = open("output_heuristic.txt", "w")
+#
+#for file in readdir("./data")
+#	println(fout, "instance " * file)
+#	P, v = heuristic("./data/" * file)
+#	if v == 0
+#		println(fout, "erreur")
+#	else
+#		println(fout, "partition = ", P)
+#		println(fout, "valeur = ", v)
+#	end
+#	println(fout, "")
+#end
+#
+#close(fout)
+#
--- main.jl
+++ main.jl
@@ -6,9 +6,10 @@ function main()
     #PL_statique("data/10_ulysses_3.tsp")
     #dualisation("data/10_ulysses_3.tsp")
     #print("z star final ", plan_coupants("data/10_ulysses_3.tsp"))
-    foreach(readdir("data_small/")) do file
-        PL_statique(string("data_small/",file))
-    end
+    
+    #foreach(readdir("data_small/")) do file
+    #    PL_statique(string("data_small/",file))
+    #end
     foreach(readdir("data_small/")) do file
         dualisation(string("data_small/",file))
     end
